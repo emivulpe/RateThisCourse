4b72fe82e8576d6c84ff6464e6bb40a9742eaeae
Michael McDonald <mcdonaldm1993@gmail.com>
2014-02-24 18:38:11 +0000

    Added Rating form

--- rate_this_course/ratethiscourse/forms.py
+++ rate_this_course/ratethiscourse/forms.py
@@ -14,3 +14,11 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('university',) #must have comma after, otherwise error (weird)
+
+
+class RatingForm(forms.ModelForm):
+	value = forms.ChoiceField(widget = forms.RadioSelect(), initial=3, choice=( (1, 'One Star'), (2, 'Two Stars'), (3, 'Three Stars'), (4, 'Four Stars'), (5, 'Five Stars') ))
+	
+	class Meta:
+		model = Rating
+		fields = ('value')
