b17b46a87aaacb942b5ef57babf98d4fd7e41f45
Michael McDonald <mcdonaldm1993@gmail.com>
2014-03-10 13:22:07 +0000

    Modified populate script to match new model

--- rate_this_course/populate_ratethiscourse.py
+++ rate_this_course/populate_ratethiscourse.py
@@ -3,72 +3,74 @@ import os
 def populate():
     glasgow_university = add_university(name = "University of Glasgow", location = "Glasgow")
 
-    add_course(university=glasgow_university,
-        name="DIM",
+    computing_science = add_course(university = glasgow_university,
+        name = "Computing Science")
+    
+    dim = add_module(university = glasgow_university,
+        course = computing_science,
+        name = "DIM",
         year = 3,
 	lecturer = "Leif")
 
-    add_course(university=glasgow_university,
-        name="PSD",
+    add_module(university = glasgow_university,
+        course = computing_science,
+        name = "PSD",
         year = 3,
 	lecturer = "Jeremy")
 
 
-    add_course(university=glasgow_university,
-        name="DB",
+    add_module(university = glasgow_university,
+        course = computing_science,
+        name = "DB",
         year = 3,
 	lecturer = "Iadh")
 
+    add_comment(message = "Very good course. Love the lecturer", module  = dim)
+    add_rating(value = 5, module = dim)
 
 
     strathclyde_university = add_university(name = "University of Strathclyde", location = "Glasgow")
 
-    management = add_course(university=strathclyde_university,
-        name="Management",
-        year = 1,
-	lecturer = "Tores")
+    add_course(university = strathclyde_university,
+        name = "Management")
 
-    add_course(university=strathclyde_university,
-        name="Marketing",
-        year = 1,
-	lecturer = "Jean")
+    add_course(university = strathclyde_university,
+        name = "Marketing")
 
-
-    add_course(university=strathclyde_university,
-        name="Psychology",
-        year = 1,
-	lecturer = "John")
-
-
-    add_comment(message="Very good course. Love the lecturer", course  = management)
-    add_rating(value = 5, course = management)
+    add_course(university = strathclyde_university,
+        name = "Psychology")
 
 
     # Print out what we have added to the user.
     for u in University.objects.all():
         for c in Course.objects.filter(university=u):
-            print "- {0} - {1}".format(str(u), str(c))
+            for m in Module.objects.filter(course=c, university=u):
+                print "-{0} -{1} -{2}".format(str(u), str(c), str(m))
 
-def add_course(university, name, year, lecturer):
-    c = Course.objects.get_or_create(university=university, name=name,year = year, lecturer = lecturer)[0]
+def add_module(university, course, name, year, lecturer):
+    m = Module.objects.get_or_create(university = university, course = course, name = name, year = year, lecturer = lecturer)[0]
+    return m
+
+def add_course(university, name):
+    c = Course.objects.get_or_create(university = university, name = name)[0]
     return c
 
 def add_university(name,location):
-    u = University.objects.get_or_create(name=name,location=location)[0]
+    u = University.objects.get_or_create(name = name, location = location)[0]
     return u
 
-def add_comment(message,course):
-    com = Comment.objects.get_or_create(message = message, course = course)[0]
+def add_comment(message,module):
+    com = Comment.objects.get_or_create(message = message, module = module)[0]
     return com
 
 
-def add_rating(value,course):
-    r = Rating.objects.get_or_create(value=value,course = course)[0]
+def add_rating(value,module):
+    r = Rating.objects.get_or_create(value = value, module = module)[0]
     return r
 
 # Start execution here!
 if __name__ == '__main__':
     print "Starting Ratemycourse population script..."
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rate_this_course.settings')
-    from ratethiscourse.models import University, Course, Rating, Comment
+    from ratethiscourse.models import University, Course, Module, Rating, Comment 
     populate()
