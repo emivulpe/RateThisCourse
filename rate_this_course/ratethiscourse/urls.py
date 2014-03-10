cdaaab79a873c3324ae5e5e4dd0be210cbe1930f
Michael McDonald <mcdonaldm1993@gmail.com>
2014-03-10 17:37:11 +0000

    Added module url

--- rate_this_course/ratethiscourse/urls.py
+++ rate_this_course/ratethiscourse/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^logout/?$', views.user_logout, name='logout'),
     url(r'^university/(?P<uni_name_url>\w+)/?$', views.university, name='university'),
     url(r'^university/(?P<uni_name_url>\w+)/(?P<course_name_url>\w+)/?$', views.course, name='course'),
+    url(r'^university/(?P<uni_name_url>\w+)/(?P<course_name_url>\w+)/(?P<module_name_url>\w+)/?$', views.module, name='module'),
     )
