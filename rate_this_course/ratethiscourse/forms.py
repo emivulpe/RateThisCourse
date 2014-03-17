a8f3555f15da9c26750b7aaf32b2d3bbacc898a3
Michael McDonald <mcdonaldm1993@gmail.com>
2014-03-17 14:59:01 +0000

    Fixed username integrity error in registration form
    - Username field displays proper error if username already exists now

--- rate_this_course/ratethiscourse/forms.py
+++ rate_this_course/ratethiscourse/forms.py
@@ -21,15 +21,15 @@ class UserForm(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ('username', 'email', 'password','confirm_password', 'first_name', 'last_name')
-	
-	def clean(self):
+
+	def clean_password(self):
 		password1 = self.cleaned_data.get('password')
 		password2 = self.cleaned_data.get('confirm_password')
 
 		if password1 and password1 != password2:
 			raise forms.ValidationError("Passwords don't match")
-
 		return self.cleaned_data
+	
 
 class UserProfileForm(forms.ModelForm):
 	university = forms.ModelChoiceField(queryset = University.objects.all(), required = True)
